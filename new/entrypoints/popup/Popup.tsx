7b3ff30860863f0a48228f6404ec248fa88823c9
rohan <rohanwagh234@gmail.com>
2024-09-23 11:46:44 +0530

    popup added

--- new/entrypoints/popup/Popup.tsx
+++ new/entrypoints/popup/Popup.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useState } from 'react';
+import reactLogo from '@/assets/react.svg';
+import wxtLogo from '/wxt.svg';
+import './App.css';
 
-export default function Popup() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className='font-xl text-mono w-full'>LinkedIn AI Reply</div>
-  )
+    <>
+      <div className='flex'>
+        <a href="https://wxt.dev" target="_blank">
+          <img src={wxtLogo} className="logo" alt="WXT logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WXT + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-doc">
+        Click on the WXT and React logos to learn more
+      </p>
+    </>
+  );
 }
+
+export default App;
